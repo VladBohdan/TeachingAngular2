eabd44fa5a3acc4d5b373e76b00544f7f52b0e40
VladBohdan <darekz04111995@gmail.com>
2020-04-27 16:54:57 +0300

    mod Put

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,60 @@
 import {Component, OnInit} from '@angular/core'
-import {HttpClient} from '@angular/common/http'
-import {delay} from 'rxjs/operators'
 import {Todo, TodosService} from "./todos.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
-  todos: Todo[] = []
+    todos: Todo[] = []
 
-  loading = false
+    loading = false
 
-  todoTitle = ''
+    todoTitle = ''
 
-  constructor(private todosService: TodosService) {}
-
-  ngOnInit() {
-    this.fetchTodos()
-  }
-
-  addTodo() {
-    if (!this.todoTitle.trim()) {
-      return
+    constructor(private todosService: TodosService) {
     }
 
-  this.todosService.addTodo({
-    title: this.todoTitle,
-    completed: false
-  }).subscribe(todo => {
-    this.todos.push(todo)
-    this.todoTitle = ''
-  })
-  }
+    ngOnInit() {
+        this.fetchTodos()
+    }
 
-  fetchTodos() {
-    this.loading = true
-    this.todosService.fetchTodos()
-      .subscribe(todos => {
-        this.todos = todos
-        this.loading = false
-      })
-  }
+    addTodo() {
+        if (!this.todoTitle.trim()) {
+            return
+        }
+
+        this.todosService.addTodo({
+            title: this.todoTitle,
+            completed: false
+        }).subscribe(todo => {
+            this.todos.push(todo)
+            this.todoTitle = ''
+        })
+    }
+
+    fetchTodos() {
+        this.loading = true
+        this.todosService.fetchTodos()
+            .subscribe(todos => {
+                this.todos = todos
+                this.loading = false
+            })
+    }
 
     removeTodo(id: number) {
         this.todosService.removeTodo(id)
-            .subscribe(() =>{
-              this.todos = this.todos.filter(t => t.id !== id)
+            .subscribe(() => {
+                this.todos = this.todos.filter(t => t.id !== id)
             })
+    }
+
+    completedTodo(id: number) {
+        this.todosService.completeTodo(id).subscribe(todo => {
+            this.todos.find(t => t.id === todo.id).completed = true
+        })
     }
 }
 
